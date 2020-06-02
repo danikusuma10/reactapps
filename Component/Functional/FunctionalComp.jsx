8acca1b4384ab37f21f548e131a663ab79107771
danikusuma10 <kusumadanikusuma@outlook.com>
2020-06-02 09:10:04 +0700

    install npm

---
+++ Component/Functional/FunctionalComp.jsx
@@ -0,0 +1,7 @@
+import React from  'react';
+
+const FungsionalComp =() => {
+    return<div>
+        <h1>hello<h1>
+            
+}
---
+++ Component/Functional/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = () => {
+    return <div>Aplikasi React JS</div>
+}
+
+export default Header()
